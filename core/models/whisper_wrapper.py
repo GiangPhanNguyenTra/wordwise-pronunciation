3ebb30d240845e9be253034e9b68aea994324898
GiangPhanNguyenTra <22520360@gm.uit.edu.vn>
2025-11-12 00:15:00 +0700

    feat: enhance whisper_wrapper

--- core/models/whisper_wrapper.py
+++ core/models/whisper_wrapper.py
@@ -1,30 +1,41 @@
 import torch 
 from transformers import pipeline
-from core.models.interfaces import IASRModel
+from .interfaces import IASRModel
 from typing import Union
 import numpy as np 
 
 class WhisperASRModel(IASRModel):
     def __init__(self, model_name="openai/whisper-base"):
-        self.asr = pipeline("automatic-speech-recognition", model=model_name, return_timestamps="word")
+        self.asr = pipeline(
+            "automatic-speech-recognition", 
+            model=model_name
+        )
         self._transcript = ""
         self._word_locations = []
         self.sample_rate = 16000
 
-    def processAudio(self, audio:Union[np.ndarray, torch.Tensor]):
-        # 'audio' can be a path to a file or a numpy array of audio samples.
+    def processAudio(self, audio: Union[np.ndarray, torch.Tensor]):
         if isinstance(audio, torch.Tensor):
             audio = audio.detach().cpu().numpy()
-        result = self.asr(audio[0])
-        self._transcript = result["text"]
-        self._word_locations = [{"word": word_info["text"], 
-                     "start_ts": word_info["timestamp"][0] * self.sample_rate if word_info["timestamp"][0] is not None else None,
-                     "end_ts": (word_info["timestamp"][1] * self.sample_rate if word_info["timestamp"][1] is not None else (word_info["timestamp"][0] + 1) * self.sample_rate),
-                     "tag": "processed"} for word_info in result["chunks"]]
+        
+        result = self.asr(
+            audio[0], 
+            return_timestamps="word",
+            generate_kwargs={"language": "english", "task": "transcribe"}
+        )
+
+        self._transcript = result.get("text", "").strip()
+        chunks = result.get("chunks", [])
+        
+        self._word_locations = [{
+            "word": word_info["text"], 
+            "start_ts": word_info["timestamp"][0] * self.sample_rate if word_info["timestamp"][0] is not None else None,
+            "end_ts": (word_info["timestamp"][1] * self.sample_rate if word_info["timestamp"][1] is not None else (word_info["timestamp"][0] + 1) * self.sample_rate),
+            "tag": "processed"
+        } for word_info in chunks]
 
     def getTranscript(self) -> str:
         return self._transcript
 
     def getWordLocations(self) -> list:
-        
         return self._word_locations
